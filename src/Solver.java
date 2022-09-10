7e78fdb7bdab0fc1649b87f184aed75ad59e99aa
Scott <smurr4@uwo.ca>
2022-09-10 00:14:27 -0400

    Added wordRemover()
    Method that removes any word from the list that doesn't match user inputs. Still in testing to be modified later

--- src/Solver.java
+++ src/Solver.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Solver {
@@ -11,20 +12,21 @@ public class Solver {
         //populates the ArrayList
         populateList(wordList);
 
-        //gets the user's guess and checks to see if it's valid
-        System.out.println(guessChecker());
+        //gets the user's guess and checks to see if it's valid and then stores it in a variable
+        String guess = guessChecker();
+
+        System.out.println(guess);
+
+        wordRemover(wordList, 'b');
+
+        System.out.println(wordList);
 
 
-
-        for (int i=0 ; i<5 ; i++){
-            String test = wordList.get(0).toString();
-            System.out.println(test.charAt(i));
-        }
 
     }
 
     //populates an ArrayList of all the possible answers
-    public static ArrayList populateList(ArrayList wordList) throws IOException{
+    public static ArrayList populateList(ArrayList wordList) throws IOException {
         //opens the file and declares a scanner to iterate through the file
         File file = new File("wordle-nyt-answers-alphabetical.txt");
         Scanner inputFile = new Scanner(file);
@@ -39,7 +41,8 @@ public class Solver {
         return wordList;
     }
 
-    public static String guessChecker(){
+    //method to make sure the guess is valid
+    public static String guessChecker() {
         //creates a String to store the users guess, a Scanner to get the input, and a boolean value as an exit condition
         String guess;
         boolean valid = false;
@@ -50,14 +53,29 @@ public class Solver {
             System.out.print("Enter your guess: ");
             guess = input.next();
 
-            if(guess.length() != 5 || !guess.matches("[a-zA-Z]+")){
+            if (guess.length() != 5 || !guess.matches("[a-zA-Z]+")) {
                 System.out.println("Invalid entry");
-            }else {
+            } else {
                 valid = true;
             }
-        }while (!valid);
+        } while (!valid);
 
         return guess;
     }
+
+    //method that removes any word that is the word of the day
+    public static void wordRemover(ArrayList wordList, char character) {
+        Iterator iterator = wordList.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().toString().charAt(0) != character) {
+                iterator.remove();
+            }
+
+        }
+    }
+
+
+
+
 }
 
